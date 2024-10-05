2132df864902d56899bf566b3d952bfa5967c608
Abdulrahman-Alsabri <Alsabri1251@gmail.com>
2024-10-05 11:34:01 +0400

    updated

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,30 +3,39 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { Component, OnInit } from '@angular/core';
-import { NbSpinnerService } from '@nebular/theme';
-import { AuthService } from './services/auth.services';
-import { MENU_ITEMS } from './components/pages/pages-menu';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { NbSpinnerService } from "@nebular/theme";
+import { AuthService } from "./services/auth.services";
+import { Subscription } from "rxjs";
+import { MENU_ITEMS } from "./components/pages/pages-menu";
 
 @Component({
-  selector: 'ngx-app',
-  templateUrl: './app.component.html',
+  selector: "ngx-app",
+  templateUrl: "./app.component.html",
 })
-export class AppComponent implements OnInit {
-
+export class AppComponent implements OnInit, OnDestroy {
   menu = MENU_ITEMS;
 
   isAuth: boolean = false;
+  private authStatusSub?: Subscription;
 
   constructor(
     private authService: AuthService,
     private spinner$: NbSpinnerService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     // this.spinner$.load();
     this.authService.autoAuthUser();
-    this.isAuth = this.authService.getIsAuth();
-    console.log(this.isAuth);
+
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe((authStatus) => {
+        this.isAuth = authStatus;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.authStatusSub?.unsubscribe();
   }
 }
--- frontend/src/app/components/pages/fr/fr-search/fr-search.component.ts
+++ frontend/src/app/components/pages/fr/fr-search/fr-search.component.ts
@@ -1,16 +1,20 @@
-import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
-import { NbComponentSize, NbMediaBreakpointsService, NbThemeService } from '@nebular/theme';
-import { SnackbarService } from '../../../../services/snackbar.service';
-import { UploadFileService } from '../../../../services/upload-file.service';
-import { map } from 'rxjs/operators';
-import { Helper } from '../../../shared/helpers';
-import { MatDialog } from '@angular/material/dialog';
-import { RolesService } from '../../../../services/roles.services';
-import { UsersService } from '../../../../services/users.services';
-import { Role } from '../../../../models/role.model';
-import { User } from '../../../../models/user.model';
-import { Subscription } from 'rxjs';
-import { DeepfakeService } from '../../../../services/deepfake.services';
+import { ChangeDetectionStrategy, Component, ViewChild } from "@angular/core";
+import {
+  NbComponentSize,
+  NbMediaBreakpointsService,
+  NbThemeService,
+} from "@nebular/theme";
+import { SnackbarService } from "../../../../services/snackbar.service";
+import { UploadFileService } from "../../../../services/upload-file.service";
+import { map } from "rxjs/operators";
+import { Helper } from "../../../shared/helpers";
+import { MatDialog } from "@angular/material/dialog";
+import { RolesService } from "../../../../services/roles.services";
+import { UsersService } from "../../../../services/users.services";
+import { Role } from "../../../../models/role.model";
+import { User } from "../../../../models/user.model";
+import { Subscription } from "rxjs";
+import { DeepfakeService } from "../../../../services/deepfake.services";
 
 interface FilePreview {
   file: File;
@@ -26,14 +30,13 @@ interface FilePreview {
 }
 
 @Component({
-  selector: 'ngx-fr-search',
-  styleUrls: ['./fr-search.component.scss'],
-  templateUrl: './fr-search.component.html',
+  selector: "ngx-fr-search",
+  styleUrls: ["./fr-search.component.scss"],
+  templateUrl: "./fr-search.component.html",
 })
 export class FrSearchComponent {
+  fixedColumnWidth = 300;
 
-  fixedColumnWidth = 0;
-  
   Helper = Helper;
 
   private rolesSub?: Subscription;
@@ -53,79 +56,90 @@ export class FrSearchComponent {
     public usersService: UsersService,
     public deepfakeService: DeepfakeService,
     private snackbarService: SnackbarService
-  ) {
-  }
+  ) {}
 
   filePreviews: FilePreview[] = [];
   currentExperiments: FilePreview[] = [];
-  actionSize: NbComponentSize = 'medium';
+  actionSize: NbComponentSize = "medium";
   isAnalyzing: boolean = false;
 
   isDragOver: boolean = false;
 
   ngOnInit() {
-  
     // Add event listeners to handle drag and drop anywhere on the page
-    window.addEventListener('dragover', this.onDragOver.bind(this), false);
-    window.addEventListener('drop', this.onDrop.bind(this), false);
-    window.addEventListener('dragleave', this.onDragLeave.bind(this), false);
+    window.addEventListener("dragover", this.onDragOver.bind(this), false);
+    window.addEventListener("drop", this.onDrop.bind(this), false);
+    window.addEventListener("dragleave", this.onDragLeave.bind(this), false);
 
     this.rolesService.getRoles();
-    this.rolesSub = this.rolesService.getRolesUpdateListener().subscribe((rolesData: any) => {
-      this.roles = rolesData;
+    this.rolesSub = this.rolesService
+      .getRolesUpdateListener()
+      .subscribe((rolesData: any) => {
+        this.roles = rolesData;
 
-      this.usersService.getUsers();
-      this.usersSub = this.usersService.getUsersUpdateListener().subscribe((usersData: any) => {
-        this.users = usersData;
+        this.usersService.getUsers();
+        this.usersSub = this.usersService
+          .getUsersUpdateListener()
+          .subscribe((usersData: any) => {
+            this.users = usersData;
+          });
       });
 
-    });
-
     const breakpoints = this.breakpointService.getBreakpointsMap();
-    this.themeService.onMediaQueryChange()
+    this.themeService
+      .onMediaQueryChange()
       .pipe(map(([, breakpoint]) => breakpoint.width))
       .subscribe((width: number) => {
-        this.actionSize = width > breakpoints.md ? 'medium' : 'small';
+        this.actionSize = width > breakpoints.md ? "medium" : "small";
       });
   }
 
   ngOnDestroy() {
     // Remove event listeners when the component is destroyed
-    window.removeEventListener('dragover', this.onDragOver.bind(this), false);
-    window.removeEventListener('drop', this.onDrop.bind(this), false);
-    window.removeEventListener('dragleave', this.onDragLeave.bind(this), false);
+    window.removeEventListener("dragover", this.onDragOver.bind(this), false);
+    window.removeEventListener("drop", this.onDrop.bind(this), false);
+    window.removeEventListener("dragleave", this.onDragLeave.bind(this), false);
 
     this.usersSub?.unsubscribe();
     this.rolesSub?.unsubscribe();
   }
 
   onDragOver(event: DragEvent) {
-    event.preventDefault();  // Prevent default to allow drop
+    event.preventDefault(); // Prevent default to allow drop
     this.isDragOver = true;
   }
-  
+
   onDrop(event: DragEvent) {
     event.preventDefault();
     this.isDragOver = false;
-    
+
     if (event.dataTransfer && event.dataTransfer.files.length > 0) {
       this.addingFiles(event.dataTransfer.files);
     }
   }
-  
+
   onDragLeave(event: DragEvent) {
     this.isDragOver = false;
   }
 
   addingFiles(files: FileList | File[]) {
     Array.from(files).forEach((file: File) => {
-      if (!file.type.startsWith('image/')) {
-        this.snackbarService.openSnackBar('غير مسموح بهذا النوع من الملفات', 'failure');
+      if (!file.type.startsWith("image/")) {
+        this.snackbarService.openSnackBar(
+          "غير مسموح بهذا النوع من الملفات",
+          "failure"
+        );
         return;
       }
 
-      if (!this.filePreviews.some(f => f.file.name === file.name && f.file.size === file.size && f.file.type === file.type)) {
-
+      if (
+        !this.filePreviews.some(
+          (f) =>
+            f.file.name === file.name &&
+            f.file.size === file.size &&
+            f.file.type === file.type
+        )
+      ) {
         var fileUrl = null;
         if (file) {
           fileUrl = URL.createObjectURL(file);
@@ -135,17 +149,20 @@ export class FrSearchComponent {
           file: file,
           url: fileUrl,
           progress: 0,
-          status: '',
+          status: "",
           isUploaded: false,
           isAnalyzed: false,
-          accuracy: '',
-          result: '',
+          accuracy: "",
+          result: "",
           videoAccuracy: 0,
-          audioAccuracy: 0
-        }
+          audioAccuracy: 0,
+        };
         this.filePreviews.push(preview);
       } else {
-        this.snackbarService.openSnackBar('يوجد ملف مُضاف بنفس الاسم. الرجاء حذفه أو تغيير اسمه.', 'failure');
+        this.snackbarService.openSnackBar(
+          "يوجد ملف مُضاف بنفس الاسم. الرجاء حذفه أو تغيير اسمه.",
+          "failure"
+        );
       }
     });
   }
@@ -156,53 +173,57 @@ export class FrSearchComponent {
 
   analyzeFiles() {
     if (this.isAnalyzing) return;
-  
+
     this.isAnalyzing = true;
-    this.filePreviews.forEach(filePreview => {
+    this.filePreviews.forEach((filePreview) => {
       if (filePreview.file) {
         this.uploadAndAnalyzeFile(filePreview);
       }
     });
     this.isAnalyzing = false;
   }
-  
+
   uploadAndAnalyzeFile(filePreview: any) {
-    this.uploadFileService.upload(filePreview.file).subscribe((fileuploadData: any) => {
-      this.updateUploadProgress(filePreview, fileuploadData);
-      
-      if (fileuploadData.result.id) {
-        this.onFileUploaded(filePreview, fileuploadData.result.id);
-      }
-    });
+    this.uploadFileService
+      .upload(filePreview.file)
+      .subscribe((fileuploadData: any) => {
+        this.updateUploadProgress(filePreview, fileuploadData);
+
+        if (fileuploadData.result.id) {
+          this.onFileUploaded(filePreview, fileuploadData.result.id);
+        }
+      });
   }
-  
+
   updateUploadProgress(filePreview: any, fileuploadData: any) {
     filePreview.status = "جاري رفع الملف...";
     filePreview.progress = fileuploadData.progress;
   }
-  
+
   onFileUploaded(filePreview: any, fileId: string) {
     filePreview.status = "تم رفع الملف!";
     filePreview.progress = 100;
     filePreview.isUploaded = true;
-  
-    if (this.filePreviews.some(preview => preview.file.name === filePreview.file.name)) {
+
+    if (
+      this.filePreviews.some(
+        (preview) => preview.file.name === filePreview.file.name
+      )
+    ) {
       this.filePreviews.splice(this.filePreviews.indexOf(filePreview), 1);
       this.currentExperiments.push(filePreview);
-  
+
       // this.analyzeFile(filePreview, fileId);
     }
   }
 
   clearFiles() {
-    if (this.isAnalyzing)
-      return;
+    if (this.isAnalyzing) return;
     this.filePreviews.splice(0, this.filePreviews.length);
   }
 
   getRoleById(id: string) {
-    const obj = this.roles.find(item => item.id === id);
-    return obj ? obj['role'] : undefined;
+    const obj = this.roles.find((item) => item.id === id);
+    return obj ? obj["role"] : undefined;
   }
-
 }
